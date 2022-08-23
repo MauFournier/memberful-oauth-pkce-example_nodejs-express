68d3a5240d2f0d4794bb5f746766f3128bbd9c8a
Mau Fournier <mau.fournier@gmail.com>
2022-08-23 17:11:24 -0600

    config: Update custom app ID

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const defaultPort = 3000;
 const signInURL = '/sign-in';
 const callbackURL = '/callback';
-const client_id = 'R9Hnp1wHuD9cg4NJcE1sgPuq'; //Your custom app's "OAuth Identifier", found in the Memberful dashboard.
+const client_id = 'AAc3JjLDMJC4TEA7AZvpTCBX'; //Your custom app's "OAuth Identifier", found in the Memberful dashboard.
 
 const generateRandomString = (length) => {
 	let text = "";
