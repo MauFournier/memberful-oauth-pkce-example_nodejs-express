197d95016054bf5f18f1aaa86d35f6c5012f5c2e
Mau Fournier <mau.fournier@gmail.com>
2022-08-25 13:58:19 -0600

    feat: Use prettier for formatting

--- index.js
+++ index.js
@@ -12,61 +12,65 @@ const memberfulSubdomain = 'INSERT_YOUR_MEMBERFUL_SUBDOMAIN_HERE'; //Your Member
 const client_id = 'INSERT_YOUR_OAUTH_IDENTIFIER_HERE'; //Your custom app's "OAuth Identifier", found in the Memberful dashboard.
 
 const generateRandomString = (length) => {
-	let text = "";
-	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	for (let i = 0; i < length; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+  let text = '';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 };
 
 const generateVerifierAndChallenge = () => {
-	const code_verifier  = generateRandomString(128);
-	const hash = crypto.createHash('sha256');
-	const hashed = hash.update(code_verifier)
-	const digested = hashed.digest();
-	const code_challenge = base64url(digested);
+  const code_verifier = generateRandomString(128);
+  const hash = crypto.createHash('sha256');
+  const hashed = hash.update(code_verifier);
+  const digested = hashed.digest();
+  const code_challenge = base64url(digested);
 
-	return {code_verifier, code_challenge};
+  return { code_verifier, code_challenge };
 };
 
 const state = generateRandomString(16);
-const {code_verifier, code_challenge} = generateVerifierAndChallenge();
+const { code_verifier, code_challenge } = generateVerifierAndChallenge();
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+app.get('/', function (req, res) {
+  res.send('Hello world!');
 });
 
-app.get(signInURL, function(req, res){
-	const response_type = "code";
-	const code_challenge_method = 'S256';
+app.get(signInURL, function (req, res) {
+  const response_type = 'code';
+  const code_challenge_method = 'S256';
 
-	//Auth code request
-	res.redirect(`${memberfulSubdomain}/oauth/?response_type=${response_type}&client_id=${client_id}&state=${state}&code_challenge=${code_challenge}&code_challenge_method=${code_challenge_method}`);
+  //Auth code request
+  res.redirect(
+    `${memberfulSubdomain}/oauth/?response_type=${response_type}&client_id=${client_id}&state=${state}&code_challenge=${code_challenge}&code_challenge_method=${code_challenge_method}`
+  );
 });
- 
-app.get(callbackURL, function(req, res){
-	const {code, state: returnedState} = req.query;
 
-	if(state === returnedState){
-		//Access token request
-		axios.post(`${memberfulSubdomain}/oauth/token`, {
-			grant_type: "authorization_code",
-			code: code,
-			client_id: client_id,
-			code_verifier: code_verifier
-		})
-		.then(function(response){
-			console.log(response.data);
-			res.send(response.data);
-		})
-		.catch(function(error){
-			console.log(error);
-			res.send(response.data);
-		});
-	} else {
-		res.send("State doesn't match");
-	}
+app.get(callbackURL, function (req, res) {
+  const { code, state: returnedState } = req.query;
+
+  if (state === returnedState) {
+    //Access token request
+    axios
+      .post(`${memberfulSubdomain}/oauth/token`, {
+        grant_type: 'authorization_code',
+        code: code,
+        client_id: client_id,
+        code_verifier: code_verifier,
+      })
+      .then(function (response) {
+        console.log(response.data);
+        res.send(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.send(response.data);
+      });
+  } else {
+    res.send("State doesn't match");
+  }
 });
 
 const PORT = process.env.PORT || defaultPort;
